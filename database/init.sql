51407b7387016ce7b6003df362f191ea819d8d80
JosePonce <practicante7@domain.com.pe>
2025-01-28 00:49:45 -0500

    api-Docker-12

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,11 @@
-CREATE DATABASE apiDocker;
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS apiDocker;
 
+-- Seleccionar la base de datos
 USE apiDocker;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'admi';
+-- Cambiar la contraseña del usuario root
+ALTER USER 'root'@'%' IDENTIFIED BY 'admi';  -- Cambia 'admi' por la contraseña que prefieras
+
+-- Refrescar los privilegios
 FLUSH PRIVILEGES;
